ac5fb81ae5207920c96ddea406027cb51970f134
haihp15 <130484610+haihp15@users.noreply.github.com>
2023-09-27 21:48:54 +0700

    add pacman movement

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,3 +1,4 @@
+using Unity.Collections.LowLevel.Unsafe;
 using UnityEngine;
 
 [RequireComponent (typeof(Rigidbody2D))]
@@ -7,7 +8,7 @@ public class Movement : MonoBehaviour
 
     public float speedMultiplier = 1.0f;
 
-    public Vector2 InitialDirection;
+    public Vector2 initialDirection;
 
     public LayerMask obstacleLayer;
     public new Rigidbody2D rigidbody {get; private set;}
@@ -21,6 +22,7 @@ public class Movement : MonoBehaviour
     private void Awake()
     {
         this.rigidbody = GetComponent<Rigidbody2D>();
+        this.startingPosition = transform.position;
     }
 
     public void Start()
@@ -29,6 +31,41 @@ public class Movement : MonoBehaviour
     }
     public void ResetState()
     {
-        
+        this.speedMultiplier = 1.0f;
+        this.direction = this.initialDirection;
+        this.nextDirection = Vector2.zero;
+        this.transform.position = this.startingPosition;
+        this.rigidbody.isKinematic = false;
+        this.enabled = true;
+    }
+    private void Update()
+    {
+        if(this.nextDirection != Vector2.zero)
+        {
+            SetDirection(this.nextDirection); 
+        }
+    }
+    private void FixedUpdate()
+    {
+        Vector2 position = this.rigidbody.position;
+        Vector2 translation = this.direction * this.speed * this.speedMultiplier * Time.fixedDeltaTime;
+        this.rigidbody.MovePosition(position + translation);
+    }
+    public void SetDirection(Vector2 direction, bool focred = false)
+    {
+        if (focred || !Occupied(direction))
+        {
+            this.direction = direction;
+            this.nextDirection = Vector2.zero;
+        }
+        else
+        {
+            this.nextDirection = direction;
+        }
+    }
+    public bool Occupied(Vector2 direction)
+    {
+        RaycastHit2D hit = Physics2D.BoxCast(this.transform.position, Vector2.one * 0.75f, 0.0f, direction, 1.5f, this.obstacleLayer);
+        return hit.collider != null;
     }
 }
